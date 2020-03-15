4f4f10cdeb55479246ceccd5443f18875f00ac9c
FernandoDurier <fernando.durier@hotmail.com>
2020-03-14 21:57:39 -0300

    added method for fitting the model

--- request.py
+++ request.py
@@ -1,4 +1,11 @@
 import requests
-url = 'http://localhost:5000/api'
-r = requests.post(url,json={'exp':1.8,})
+url = 'http://localhost:5000/'
+r = requests.post(url+'predict',json={'exp':1.8,})
+print(r.json())
+d = {
+    "features":[3.,4.,5.,6.],
+    "labels": [2000,4000,8000,16000]
+}
+r = requests.post(url+'fit',json=d)
+r = requests.post(url+'predict',json={'exp':4,})
 print(r.json())
--- server.py
+++ server.py
@@ -5,13 +5,23 @@ import pickle
 app = Flask(__name__)# Load the model
 model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/api',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     # Get the data from the POST request.
     data = request.get_json(force=True)    # Make prediction using model loaded from disk as per the data.
     prediction = model.predict([[np.array(data['exp'])]])    # Take the first value of prediction
     output = prediction[0]    
     return jsonify(output)
+
+@app.route('/fit',methods=['POST'])
+def fit():
+    data = request.get_json(force=True)
+    X = data['features']
+    y = data['labels']    
+    model.fit(X,y)
+    pickle.dump(regressor, open('model.pkl','wb'))# Loading model to compare the results
+    model = pickle.load(open('model.pkl','rb'))
+    return "Model trained."
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
